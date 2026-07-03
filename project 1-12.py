c4b7f8eaf8b681670752c683906f60aa91b46b7e
Iftekhar Habib Samad <iftekharhabibsamad@gmail.com>
2026-07-04 01:11:00 +0600

    Add files via upload

---
+++ project 1-12.py
@@ -0,0 +1,51 @@
+#1.Hello World
+print("Hello world")
+#2.My Name
+print("My name is Iftekhar Habib Samad")
+#3.My parmanent Address
+print("Vill: Chachuria 08no Ward,")
+print("Post: K.b.bazar-4391, Bailchhari")
+#4.My Name,Age
+print("Name:Iftekhar Habib Samad")
+print("AGE:17 yars old")
+print("District:Chattogram")
+print("Coutry:Bangladesh")
+#5.Demonstrating escape characters
+print("Course: Python Programming")
+print("Topic: Escape Characters")
+print("Status: Completed")
+print("Path:\tC:\\Users\\Python\\Notes")
+#6.Printing a formatted date using sep parameter
+print("2026", "07", "01", sep="-")
+
+# Printing items separated by arrow
+print("Dhaka", "Chittagong", "Sylhet", sep=" -> ")
+#7.Printing multiple statements on the same line using end parameter
+print("Hello", end=" ")
+print("World!", end=" ")
+print("Welcome to Python.")
+#8.two numbers from user and finding their sum
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+total = num1 + num2
+print("The sum is:", total)
+#9.two numbers from user and finding their sub
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+sub = num1 - num2
+print("The sub is:", sub)
+#10.two numbers from user and finding their mul
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+mul = num1 * num2
+print("The mul is:", mul)
+#11.two numbers from user and finding their div
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+div = num1 / num2
+print("The div is:", div)
+#12.two numbers from user and finding their mod
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+mod = num1 % num2
+print("The mod is:", mod)
