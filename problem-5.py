ca0c99d8fd86ced7d4d416727021270109fc0cee
Iftekhar Habib Samad <iftekharhabibsamad@gmail.com>
2026-07-12 06:41:41 +0600

    Add files via upload

--- problem-5.py
+++ problem-5.py
@@ -1,5 +1,5 @@
 #5.Demonstrating escape characters
-print("Course: Python Programming")
-print("Topic: Escape Characters")
-print("Status: Completed")
-print("Path:\tC:\\Users\\Python\\Notes")
+print("\nAssalamualaikum Banglades")
+print("Topic:\tEscape Characters")
+print("Status:\"Completed\" ")
+print("Path:\tC:\\Users\\Python Notes")
